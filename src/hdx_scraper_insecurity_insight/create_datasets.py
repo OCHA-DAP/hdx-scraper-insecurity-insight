b867101a7aa953c03f0b0f5321c78e94e1e3031f
Ian Hopkinson <ianhopkinson@googlemail.com>
2023-11-24 11:49:57 +0000

    Drive dataset creation from attributes.csv

--- src/hdx_scraper_insecurity_insight/create_datasets.py
+++ src/hdx_scraper_insecurity_insight/create_datasets.py
@@ -3,7 +3,9 @@
 
 import logging
 import os
-import sys
+import re
+
+from pathlib import Path
 
 from hdx.utilities.easy_logging import setup_logging
 from hdx.api.configuration import Configuration
@@ -32,12 +34,18 @@ def create_datasets_in_hdx(dataset_name):
             f"`{dataset_name}` does not exist in hdx_site: `{Configuration.read().hdx_site}`"
         )
 
-    # Read API for date range and countries (use the full data rather than the Overview, although the Overview has a longer timescale)
-    resource_dataset_name = "insecurity-insight-crsv"
+    dates = []
+    countries = []
+    resource_names = dataset_attributes["resource"]
+    for resource_dataset_name in resource_names:
+        resource_json = fetch_json_from_api(resource_dataset_name)
+        date_field = "Date"
+        if date_field not in resource_json[0].keys():
+            date_field = "Year"
+        for row in resource_json:
+            dates.append(row[date_field])
+            countries.append(row["Country ISO"].lower())
 
-    resource_json = fetch_json_from_api(resource_dataset_name)
-
-    dates = [x["Date"] for x in resource_json]
     start_date = min(dates)
     end_date = max(dates)
 
@@ -45,36 +53,54 @@ def create_datasets_in_hdx(dataset_name):
     LOGGER.info(f"Dataset_date: {dataset_date}")
 
     # Possibly we want to run a counter here to work out the significant countries in the dataset
-    countries = {x["Country ISO"].lower() for x in resource_json}
-
     countries_group = [{"name": x} for x in countries]
-
     LOGGER.info(f"Data from {len(countries_group)} countries found")
 
     dataset_template_filepath = os.path.join(
-        os.path.dirname(__file__), "new-dataset-templates", "conflict-related-sexual-violence.json"
+        os.path.dirname(__file__), "new-dataset-templates", dataset_attributes["dataset_template"]
     )
 
     dataset = Dataset.load_from_json(dataset_template_filepath)
 
-    # dataset_date - this needs updated according to the data
     dataset["dataset_date"] = dataset_date
-    # Need to add groups on the basis of data
     dataset["groups"] = countries_group
-    # Does license_url and license_title get generated from license_id
 
     # What do we do about quickcharts?
-    resource_names = ["insecurity-insight-crsv", "insecurity-insight-crsv-overview"]
-
     resource_list = []
 
+    spreadsheet_directory = os.path.join(os.path.dirname(__file__), "output-spreadsheets")
     for resource_name in resource_names:
         attributes = read_attributes(resource_name)
-        country_iso = ""
-        if resource_name == "insecurity-insight-crsv":
-            filename = "2016-2022-conflict-related-sexual-violence-crsv-overview-data.xlsx"
-        elif resource_name == "insecurity-insight-crsv-overview":
-            filename = "2020-2023-conflict-related-sexual-violence-crsv-incident-data.xlsx"
+        # this regex will fail on spreadsheets with an country ISO code in the filename
+        # And also single year spreadsheets
+        spreadsheet_regex = (
+            attributes["filename_template"]
+            .replace("{start_year}", "[0-9]{4}")
+            .replace("{end_year}", "[0-9]{4}")
+            .replace("{country_iso}", "")
+        )
+
+        file_list = Path(spreadsheet_directory)
+        files = []
+        for file_ in file_list.iterdir():
+            matching_files = re.search(spreadsheet_regex, str(file_))
+            if matching_files is not None:
+                files.append(matching_files.group())
+
+        if len(files) == 0:
+            LOGGER.error(
+                f"No spreadsheets matching {spreadsheet_regex} found in `output-spreadsheets`"
+            )
+            raise FileNotFoundError
+        elif len(files) == 1:
+            filename = files[0]
+            LOGGER.info(f"`{filename}` found for resource `{resource_name}`")
+        else:
+            LOGGER.error(
+                f"Multiple (n={len(files)}) spreadsheets matching {spreadsheet_regex}"
+                " found in `output-spreadsheets`"
+            )
+            raise FileNotFoundError
 
         resource = Resource(
             {
@@ -85,16 +111,15 @@ def create_datasets_in_hdx(dataset_name):
         )
         resource.set_file_to_upload(
             os.path.join(
-                os.path.dirname(__file__),
-                "output-spreadsheets",
+                spreadsheet_directory,
                 filename,
             )
         )
         resource_list.append(resource)
 
     dataset.add_update_resources(resource_list)
-    print(dataset, flush=True)
-    dataset.create_in_hdx()
+    # print(dataset, flush=True)
+    # dataset.create_in_hdx()
 
 
 if __name__ == "__main__":
