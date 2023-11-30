902fa6ff5d70d1a022168751d6b5d311fc88249e
Ian Hopkinson <ianhopkinson@googlemail.com>
2023-11-30 12:58:06 +0000

    Tidy up / rename read_hdx. Testing seems impossible and not particularly relevant

--- src/hdx_scraper_insecurity_insight/read_insecurity_insight_hdx.py
+++ src/hdx_scraper_insecurity_insight/read_insecurity_insight_hdx.py
@@ -19,16 +19,9 @@ logger = logging.getLogger(__name__)
 Configuration.create(hdx_site="prod", user_agent="hdxds_insecurity_insight", hdx_read_only=True)
 
 
-def main():
+def create_insecurity_insight_resource_list():
     organization = Organization.read_from_hdx("insecurity-insight")
-    # for k, v in organization.items():
-    #     print(f"{k}: {v}", flush=True)
-    # print(organization, flush=True)
-
     datasets = organization.get_datasets()
-
-    # for k, v in datasets[0].items():
-    #     print(f"{k}: {v}", flush=True)
 
     n_datasets = len(datasets)
     n_resources = 0
@@ -36,28 +29,19 @@ def main():
     resources_dict = {}
     for dataset in datasets:
         latest_date = dataset["dataset_date"][24:34]
-        # print(
-        #     f"{dataset['name']}:{latest_date}({dataset['num_resources']})",
-        #     flush=True,
-        # )
         resources = Dataset.get_resources(dataset)
-        # print(resources, flush=True)
         for resource in resources:
-            # print(f"\t{resource['name']}", flush=True)
             if dataset["name"] not in resources_dict:
                 resources_dict[dataset["name"]] = [resource["name"]]
             else:
                 resources_dict[dataset["name"]].append(resource["name"])
-            # for k, v in resource.items():
-            #     print(f"\t{k}: {v}", flush=True)
-            # break
         n_resources += dataset["num_resources"]
         summary.append([dataset["title"], dataset["name"], latest_date, dataset["num_resources"]])
-        dataset.save_to_json(
-            os.path.join(
-                os.path.dirname(__file__), "legacy-dataset-json", f"{dataset['name']}.json"
-            )
-        )
+        # dataset.save_to_json(
+        #     os.path.join(
+        #         os.path.dirname(__file__), "legacy-dataset-json", f"{dataset['name']}.json"
+        #     )
+        # )
 
     summary.sort(key=lambda x: x[2], reverse=True)
 
@@ -70,13 +54,8 @@ def main():
     logger.info(f"Number of datasets: {n_datasets}")
     logger.info(f"Number of resources: {n_resources}")
 
-    # dataset = Dataset.read_from_hdx("novel-coronavirus-2019-ncov-cases")
-    # for method in dir(dataset):
-    #     print(method, flush=True)
-
-    # for k, v in dataset.items():
-    #     print(f"{k}: {v}", flush=True)
+    return n_datasets, n_resources
 
 
 if __name__ == "__main__":
-    main()
+    n_datasets, n_resources = create_insecurity_insight_resource_list()
